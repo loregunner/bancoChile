b2a98d8e21c5ef78ab46232544432489854c331f
loregunner <dahara59@gmail.com>
2020-11-27 13:25:28 -0500

    Updates

---
+++ Access/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+
+const config = {    
+    apiKey: "AIzaSyBt-FIM7wKaXPBGcuasNW9n-FGCPFS2N_g",
+    authDomain: "bancochilefirst.firebaseapp.com",
+    databaseURL: "https://bancochilefirst.firebaseio.com",
+    projectId: "bancochilefirst",
+    storageBucket: "bancochilefirst.appspot.com",
+    messagingSenderId: "1054194570281",
+    appId: "1:1054194570281:web:96a23ceeb3b053d2a1b838"
+};
+
+firebase.initializeApp(config);
+
+export default firebase;
---
+++ App.js
@@ -0,0 +1,61 @@
+import logo from './logo.svg';
+import './App.css';
+//import { Component } from 'react';
+//import Login from './pages/Login';
+//import Login from './pages/Login';
+//import VistaPrincipal from './pages/vistaPrincipal';
+import Router from './Router';
+
+export default class App extends Component {
+  render(){
+    <Router />
+    return(
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+      </div>
+    );
+  }
+}
+
+/* export default function App() {
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+    </header>
+  </div>
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Login</Link>
+            </li>
+            <li>
+              <Link to="/vistaPrincipal">VistaPrincipal</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/">
+            <Login />
+          </Route>
+          <Route path="/vistaPrincipal">
+            <VistaPrincipal />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+Login = () => {
+  return <h2>Home</h2>;
+}
+
+VistaPrincipal = () => {
+  return <h2>About</h2>;
+}
+ */
